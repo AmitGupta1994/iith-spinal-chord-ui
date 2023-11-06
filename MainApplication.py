0d4d707bc0c465eb74a898a79dc0eef350e80091
Amit Gupta <iamitgupta1994@gmail.com>
2023-11-07 02:41:21 +0530

    [success]

--- MainApplication.py
+++ MainApplication.py
@@ -17,21 +17,171 @@ class MainApplication(QMainWindow):
 
         self.setWindowTitle("Spinal Chord")
 
-
+        # C5
         self.segment_c5_muscles = [
-            SpinalChordMuscle('ercspn_l','Erector Spinae Left'), SpinalChordMuscle('ercspn_r','Erector Spinae Right'),
-            SpinalChordMuscle('DELT1','Deltoid 1 Right'), SpinalChordMuscle('DELT2','Deltoid 1 Left'),
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
             SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
 
         ]
-        self.segment_c5 = SpinalChordSegment("C5",self.segment_c5_muscles, [10, 20])
+        self.segment_c5 = SpinalChordSegment("C5", self.segment_c5_muscles)
 
+        # C6
         self.segment_c6_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_c6 = SpinalChordSegment("C6", self.segment_c6_muscles)
+
+        # C7
+        self.segment_c7_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_c7 = SpinalChordSegment("C7", self.segment_c7_muscles)
+
+        # C8
+        self.segment_c8_muscles = [
             SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
             SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
 
         ]
-        self.segment_c6 = SpinalChordSegment("C6",self.segment_c6_muscles,[0,100])
+        self.segment_c8 = SpinalChordSegment("C8", self.segment_c8_muscles)
+
+        # T7-T12
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_t7_t12 = SpinalChordSegment("T7-T12", self.segment_c8_muscles)
+
+        # L1
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_l1= SpinalChordSegment("L1", self.segment_c8_muscles)
+
+        # L2
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_l2 = SpinalChordSegment("L2", self.segment_c8_muscles)
+
+        # L3
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_l3= SpinalChordSegment("L3", self.segment_c8_muscles)
+
+        # L4
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_l4 = SpinalChordSegment("L4", self.segment_c8_muscles)
+
+        # L5
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_l5 = SpinalChordSegment("L5", self.segment_c8_muscles)
+
+        # S1
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_s1 = SpinalChordSegment("S1", self.segment_c8_muscles)
+
+        # S2
+        self.segment_c8_muscles = [
+            SpinalChordMuscle('ercspn_l', 'Erector Spinae Left'), SpinalChordMuscle('ercspn_r', 'Erector Spinae Right'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+            SpinalChordMuscle('DELT1', 'Deltoid 1 Right'), SpinalChordMuscle('DELT2', 'Deltoid 1 Left'),
+
+        ]
+        self.segment_s2 = SpinalChordSegment("S2", self.segment_c8_muscles)
 
         ####################################
         # Create a layout for the main window
@@ -39,33 +189,42 @@ class MainApplication(QMainWindow):
 
         main_layout = QVBoxLayout()
 
-        self.segments.append(self.segment_c5)
-        self.segments.append(self.segment_c6)
-
-        main_layout.addWidget(self.segment_c5)
-        main_layout.addWidget(self.segment_c6)
-
-        # Set the main layout for the main window
         central_widget = QWidget()
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
+        self.segments.append(self.segment_c5)
+        self.segments.append(self.segment_c6)
+        self.segments.append(self.segment_c7)
+        self.segments.append(self.segment_c8)
+        self.segments.append(self.segment_c8)
+        self.segments.append(self.segment_t7_t12)
+        self.segments.append(self.segment_l1)
+        self.segments.append(self.segment_l2)
+        self.segments.append(self.segment_l3)
+        self.segments.append(self.segment_l4)
+        self.segments.append(self.segment_l5)
+        self.segments.append(self.segment_s1)
+        self.segments.append(self.segment_s2)
 
-        ####################################
-        # Create a layout for the main window
-        main_layout = QHBoxLayout()
-
-        # Add the vertical rectangles widget to the left side
-        # main_layout.addWidget(self.segment_c5)
+        main_layout.addWidget(self.segment_c5)
         main_layout.addWidget(self.segment_c6)
+        main_layout.addWidget(self.segment_c7)
+        main_layout.addWidget(self.segment_c8)
+        main_layout.addWidget(self.segment_t7_t12)
+        main_layout.addWidget(self.segment_l1)
+        main_layout.addWidget(self.segment_l2)
+        main_layout.addWidget(self.segment_l3)
+        main_layout.addWidget(self.segment_l4)
+        main_layout.addWidget(self.segment_l5)
+        main_layout.addWidget(self.segment_s1)
+        main_layout.addWidget(self.segment_s2)
 
         # Add the UI loaded from the .ui file to the main layout
         main_layout.addWidget(self.centralWidget())
 
         # Set the main layout for the main window
-        central_widget = QWidget()
-        central_widget.setLayout(main_layout)
-        self.setCentralWidget(central_widget)
+
 
     def initialize_ui(self):
         # self.ui.layout.addWidget(self.segment_c5)
--- SpinalChordSegment.py
+++ SpinalChordSegment.py
@@ -5,35 +5,33 @@ from PyQt5.QtWidgets import QWidget
 
 class SpinalChordSegment(QWidget):
 
-    def __init__(self,name,muscles_name_list,position):
+    def __init__(self, name, muscles_name_list):
         super().__init__()
-
-        self.setFixedWidth(80)  # Adjust the width as needed
-        self.setAutoFillBackground(True)
-        self.circle_color = QColor(0, 0, 255)  # Default circle color
-
-        self.num_rows = int(len(muscles_name_list)/2)
 
         self.name = name
         self.muscles_name_list = muscles_name_list
-        self.position = position
 
-        # self.draw_segment()
-
-        segment_x = self.position[0]
-        segment_y = self.position[1]
-
-        print("x,y",segment_x,segment_y)
-
-        self.rect_x = segment_x
-        self.rect_y = segment_y
-        self.rect_width = 200
+        self.rect_width = 400
 
         # Calculate the height of the rectangle based on the number of circles
-        circle_radius = 5
-        circle_spacing = 10
-        num_circles = len(muscles_name_list)/2
-        self.rect_height = 2 * (circle_radius + circle_spacing) * (num_circles // 2)
+        self.circle_radius = 2
+        self.circle_spacing = 7
+        self.num_circles = 2
+        # self.circle_y = 0
+        self.circle_y = 4
+        num_of_rows = len(muscles_name_list) / 2 + 1
+
+        print('num_of_rows', num_of_rows)
+
+        self.rect_height = int(2 * (self.circle_radius + self.circle_spacing) * (num_of_rows // 2))
+
+        self.setFixedWidth(self.rect_width)  # Adjust the width as needed
+        self.setFixedHeight(self.rect_height)  # Adjust the width as needed
+
+        self.setAutoFillBackground(True)
+        self.circle_color = QColor(0, 0, 0)  # Default circle color
+
+        self.num_rows = int(len(muscles_name_list) / 2)
 
     def setCircleColor(self, color):
         self.circle_color = color
@@ -41,25 +39,132 @@ class SpinalChordSegment(QWidget):
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        rect_width = 200
-        rect_height = self.rect_height
 
-        painter.setBrush(QBrush(QColor(255, 0, 0)))  # Change the color of the rectangle as needed
-        painter.drawRect(self.rect_x, self.rect_y, rect_width, rect_height)
+        painter.setBrush(QBrush(QColor(255, 255, 255)))  # Change the color of the rectangle as needed
+        painter.drawRect(0, 0, self.width(), self.height())
+
+        font = QFont("Arial", 10)  # Adjust the font as needed
+        painter.setFont(font)
+        text_rect = painter.boundingRect(self.rect(), 0, self.name)
+        text_x = (self.width() - text_rect.width()) / 2
+        text_y = (self.height() + text_rect.height()) / 2
+        painter.drawText(text_x, text_y, self.name)
+
+        # For Circles Inside
+        # Calculate circle properties
+
+        # Draw circles in rows
+        count = 0
+        # for row in range(self.num_rows):
+        #     circle_x = self.circle_spacing + self.circle_radius
+        #     for _ in range(self.num_circles):
+        #         painter.setBrush(QBrush(self.circle_color))  # Circle color
+        #         painter.drawEllipse(circle_x, self.circle_y + row * self.height() / self.num_rows,
+        #                             2 * self.circle_radius,
+        #                             2 * self.circle_radius)
+        #
+        #         # # Draw the circle name text below the circle
+        #         # circle_name = self.muscles_name_list[count].name
+        #         # circle_name_font = QFont("Arial", 8)  # Adjust the font as needed
+        #         # painter.setFont(circle_name_font)
+        #         # circle_name_rect = painter.boundingRect(self.rect(), 0, circle_name)
+        #         # circle_name_x = circle_x - (circle_name_rect.width() - 2 * self.circle_radius) / 2
+        #         # circle_name_y = self.circle_y + 2 * self.circle_radius + row * self.height() / self.num_rows + circle_name_rect.height()
+        #         # painter.drawText(circle_name_x, circle_name_y, circle_name)
+        #         #
+        #         # circle_x += 2 * (self.circle_radius + self.circle_spacing)
+        #
+        #         # Draw the same text at the side of the circle
+        #         side_text = self.muscles_name_list[count].name
+        #         side_text_font = QFont("Arial", 10)  # Adjust the font as needed
+        #         painter.setFont(side_text_font)
+        #         side_text_x = circle_x + 2 * self.circle_radius + self.circle_spacing
+        #         side_text_y = self.circle_y + row * self.height() / self.num_rows + self.circle_radius
+        #         painter.drawText(side_text_x, side_text_y, side_text)
+        #
+        #         circle_x += 2 * (self.circle_radius + self.circle_spacing)
+        #
+        #         count = count + 1
+
+        # for row in range(self.num_rows):
+        #     circle_x = self.circle_spacing + self.circle_radius
+        #
+        #     # Draw the name of the first circle
+        #     circle_name = self.muscles_name_list[count].name
+        #     circle_name_font = QFont("Arial", 8)  # Adjust the font as needed
+        #     painter.setFont(circle_name_font)
+        #     circle_name_rect = painter.boundingRect(self.rect(), 0, circle_name)
+        #     circle_name_x = circle_x - (circle_name_rect.width() - 2 * self.circle_radius) / 2
+        #     circle_name_y = self.circle_y + 2 * self.circle_radius + circle_name_rect.height() + row * self.height() / self.num_rows
+        #     painter.drawText(circle_name_x, circle_name_y, circle_name)
+        #
+        #     # Draw the first circle
+        #     painter.setBrush(QBrush(QColor(0, 0, 255)))  # Circle color
+        #     painter.drawEllipse(circle_x, self.circle_y + row * self.height() / self.num_rows, 2 * self.circle_radius,
+        #                         2 * self.circle_radius)
+        #
+        #     # Draw the name of the second circle
+        #     circle_name = self.muscles_name_list[count+1].name
+        #     circle_name_font = QFont("Arial", 8)  # Adjust the font as needed
+        #     painter.setFont(circle_name_font)
+        #     circle_name_rect = painter.boundingRect(self.rect(), 0, circle_name)
+        #     circle_name_x = circle_x - (circle_name_rect.width() - 2 * self.circle_radius) / 2
+        #     circle_name_y = self.circle_y + 2 * self.circle_radius + circle_name_rect.height() + row * self.height() / self.num_rows
+        #     painter.drawText(circle_name_x + 2 * (self.circle_radius + self.circle_spacing), circle_name_y, circle_name)
+        #
+        #     # Draw the second circle
+        #     painter.setBrush(QBrush(QColor(0, 0, 255)))  # Circle color
+        #     painter.drawEllipse(circle_x + 2 * (self.circle_radius + self.circle_spacing),
+        #                         self.circle_y + row * self.height() / self.num_rows, 2 * self.circle_radius, 2 * self.circle_radius)
+        #
+        #     count = count + 1
 
         for row in range(self.num_rows):
-            y = row * rect_height
-            # painter.setBrush(QBrush(QColor(255, 0, 0)))  # Change the color of the rectangle as needed
-            # painter.drawRect(self.rect_x, self.rect_y, rect_width, rect_height)
+            circle_x = self.circle_spacing + self.circle_radius + 100
 
-            # Draw circles inside the rectangle
-            circle_radius = min(rect_width, rect_height) // 4
-            circle_spacing = 10
-            circle_x = circle_radius + circle_spacing
-            circle_y = y + rect_height // 2
+            # Draw the name of the first circle to the left of the circle
+            circle_name = self.muscles_name_list[count].desc
+            circle_name_font = QFont("Arial", 6)  # Adjust the font as needed
+            painter.setFont(circle_name_font)
+            circle_name_rect = painter.boundingRect(self.rect(), 0, circle_name)
+            circle_name_x = circle_x - (circle_name_rect.width() - 2 * self.circle_radius) / 2
+            circle_name_y = self.circle_y + row * self.height() / self.num_rows + self.circle_y
+            painter.drawText(circle_name_x - circle_name_rect.width(), circle_name_y, circle_name)
 
-            for _ in range(2):
-                painter.setBrush(QBrush(self.circle_color))  # Use the dynamic circle color
-                painter.drawEllipse(circle_x - circle_radius, circle_y - circle_radius, circle_radius * 2,
-                                    circle_radius * 2)
-                circle_x += 2 * (circle_radius + circle_spacing)
+            # Draw the first circle
+            painter.setBrush(QBrush(QColor(0, 0, 255)))  # Circle color
+            painter.drawEllipse(circle_x, self.circle_y + row * self.height() / self.num_rows, 2 * self.circle_radius,
+                                2 * self.circle_radius)
+
+            # Draw the name of the second circle to the right of the circle
+            circle_name = self.muscles_name_list[count+1].desc
+            circle_name_font = QFont("Arial", 6)  # Adjust the font as needed
+            painter.setFont(circle_name_font)
+            circle_name_rect = painter.boundingRect(self.rect(), 0, circle_name)
+            circle_name_x = circle_x - (circle_name_rect.width() - 2 * self.circle_radius) / 2
+            circle_name_y = self.circle_y + row * self.height() / self.num_rows + self.circle_y
+            painter.drawText(circle_x + 20 * (self.circle_radius + self.circle_spacing), circle_name_y, circle_name)
+
+            # Draw the second circle
+            painter.setBrush(QBrush(QColor(0, 0, 255)))  # Circle color
+            painter.drawEllipse(circle_x + 18 * (self.circle_radius + self.circle_spacing),
+                                self.circle_y + row * self.height() / self.num_rows, 2 * self.circle_radius, 2 * self.circle_radius)
+
+            count = count + 1
+
+        # for row in range(self.num_rows):
+        #     y = row * self.height()
+        #     # painter.setBrush(QBrush(QColor(255, 0, 0)))  # Change the color of the rectangle as needed
+        #     # painter.drawRect(self.rect_x, self.rect_y, rect_width, rect_height)
+        #
+        #     # Draw circles inside the rectangle
+        #     circle_radius = min(self.width(), self.height()) // 4
+        #     circle_spacing = 10
+        #     circle_x = circle_radius + circle_spacing
+        #     circle_y = y + self.height() // 2
+        #
+        #     for _ in range(2):
+        #         painter.setBrush(QBrush(self.circle_color))  # Use the dynamic circle color
+        #         painter.drawEllipse(circle_x - circle_radius, circle_y - circle_radius, circle_radius * 2,
+        #                             circle_radius * 2)
+        #         circle_x += 2 * (circle_radius + circle_spacing)
